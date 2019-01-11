7776cd3ab5062dae48811f41f88d2811da14c858
Harley Rayner <harleymteden@gmail.com>
2019-01-11 22:45:27 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,7 @@ class StudentsController < ApplicationController
 
   def create 
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path
   end 
 
   def show
